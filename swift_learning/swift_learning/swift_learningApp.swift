e33e628f9a0c8e109bdbc5b964d516b1fb9391a9
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2024-09-25 11:00:09 -0500

    so

---
+++ swift_learning/swift_learning/swift_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learningApp.swift
+//  swift_learning
+//
+//  Created by Petr Kolesnikov on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
